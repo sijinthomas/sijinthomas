81bef2818c5ebf8ab694f5f9823d13b382fc764b
Sijin Thomas <sijinthomaspalliyady@gmail.com>
2024-02-01 12:07:11 +0000

    Bump version to

--- app.ts
+++ app.ts
@@ -65,7 +65,7 @@ app.configure({
       },
       components: {
          'com.revvity.streamrelay': {
-            version: '1.0.13',
+            version: '',
             publish: {
                repository: 'greengrass-component-com-revvity-streamrelay',
             },
